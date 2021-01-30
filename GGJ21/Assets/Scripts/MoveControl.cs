c912ad39f697edfa8cd241065d8ef28a06df1015
whateverton <evertonandradevilela@gmail.com>
2021-01-30 02:15:52 -0300

    Prototyping controls

---
+++ GGJ21/Assets/Scripts/MoveControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputVec = context.action.ReadValue<Vector2>();
+
+        Debug.Log("Moved!");
+        Debug.Log(inputVec);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+            Debug.Log("Jump");
+    }
+}
