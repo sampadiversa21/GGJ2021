84cac41b7b0a1156ffa2c1b89a594cfa4a72947a
whateverton <evertonandradevilela@gmail.com>
2021-01-30 12:25:20 -0300

    Basic movement script

--- GGJ21/Assets/Scripts/MoveControl.cs
+++ GGJ21/Assets/Scripts/MoveControl.cs
@@ -5,29 +5,77 @@ using UnityEngine.InputSystem;
 
 public class MoveControl : MonoBehaviour
 {
+    private Rigidbody2D myRigidbody2D;
+
+    private bool jumped;
+    private int jumpNumber;
+
+    private bool isGrounded;
+
+    private Vector2 inputDirection;
+
+    [SerializeField]
+    private float speed = 4.0f;
+
+    [SerializeField]
+    private int jumpHeight = 5;
+
+    [SerializeField]
+    private int maxJump = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGrounded = true;
+        jumped = false;
+        jumpNumber = 0;
+        myRigidbody2D = GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 inputVec = context.action.ReadValue<Vector2>();
-
-        Debug.Log("Moved!");
-        Debug.Log(inputVec);
+        inputDirection = context.action.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.phase == InputActionPhase.Performed)
-            Debug.Log("Jump");
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if(jumpNumber < maxJump)
+                jumped = true;
+        }
     }
+
+    protected void HandleMovement()
+    {
+        myRigidbody2D.AddForce(new Vector2(Time.deltaTime * speed * inputDirection.x * 100f, 0), ForceMode2D.Force);
+
+        if (jumpNumber > 0 && isGrounded)
+        {
+            jumpNumber = 0;
+        }
+
+        if (jumped && jumpNumber < maxJump)
+        {
+            jumpNumber++;
+            jumped = false;
+            isGrounded = false;
+            myRigidbody2D.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
 }
