7cbf44433c2a509e2f2021fe5520e1fc089e92eb
Gustavo Vizoni <gustavo.vizoni@gmail.com>
2021-01-31 17:38:21 -0300

    added frog behavior

---
+++ GGJ21/Assets/ExposeAnimTriggers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExposeAnimTriggers : MonoBehaviour
+{
+    public Animator animator;
+
+    public void SetTrigger(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+
+    public void SetTrigger(int id)
+    {
+        animator.SetTrigger(id);
+    }
+
+    public void SetBool(string id, bool value)
+    {
+        animator.SetBool(id, value);
+    }
+
+    public void SetFloat(string id, float value)
+    {
+        animator.SetFloat(id, value);
+    }
+}
