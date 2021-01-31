4add7dcbeb6cefa038563df66629ad8b55732fab
whateverton <evertonandradevilela@gmail.com>
2021-01-31 02:20:26 -0300

    Creating breakable door

---
+++ GGJ21/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,23 @@
+﻿using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.gameObject.GetComponent<PlayerController>().isPecking)
+            {
+                GetComponent<Animator>().SetBool("break", true);
+            }
+        }
+    }
+
+    public void FinishAnimation()
+    {
+        Destroy(gameObject);
+    }
+}
